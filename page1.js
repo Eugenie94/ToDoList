6f5916ae05f66c41aea64423d82dcf6dd64b9e08
Eugenie94 <engie94400@gmail.com>
2020-09-23 06:55:25 +0200

    gros tafffff

---
+++ page1.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {    /*permet d'augmenter ma progress barre lorsque l'on coche */
+  
+    // get box count
+    var count = 0;   
+    var checked = 0;
+    function countBoxes() { 
+      count = $("input[type='checkbox']").length;
+      console.log(count);
+    }
+    
+    countBoxes();
+    $(":checkbox").click(countBoxes);
+    
+    // count checks
+    
+    function countChecked() {
+      checked = $("input:checked").length;
+      
+      var percentage = parseInt(((checked / count) * 100),10);
+      $(".progressbar-bar").progressbar({
+              value: percentage
+          });
+      $(".progressbar-label").text(percentage + "%");
+    }
+    
+    countChecked();
+    $(":checkbox").click(countChecked);
+  });
+
+  if (checkbox == "")
+{
+alert("Félicitations !");
+}
+
+
+   /* window.alert("Félicitations"); */
+  
+
+ 
