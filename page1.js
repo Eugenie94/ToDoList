3964b83b5f6cbfc9d8057448f27955009a6dbacf
Eugenie94 <engie94400@gmail.com>
2020-09-24 01:23:12 +0200

    ajout couleur progress bar

--- page1.js
+++ page1.js
@@ -21,6 +21,19 @@ $(document).ready(function() {    /*permet d'augmenter ma progress barre lorsque
               value: percentage
           });
       $(".progressbar-label").text(percentage + "%"); 
+      var elem = document.getElementById("mam");
+      elem.style.width = percentage + "%";
+
+          if(percentage<25){
+            elem.style.backgroundColor = "red"
+          }else{
+            if(percentage<50){
+              elem.style.backgroundColor = "orange"
+            }else{
+              elem.style.backgroundColor = "green"
+            }
+          }
+
       if(percentage==100){setTimeout(function(){alert("félicitations");}, 1000);}  /* si le pourcentage arrive a 100% un pop up felicitation apparait au bout de 1s*/
     }
     
