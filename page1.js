9c4480bfadc3e08159f94d28162b3ac73c12c6ef
Eugenie94 <engie94400@gmail.com>
2020-09-24 07:41:50 +0200

    fondecran

--- page1.js
+++ page1.js
@@ -35,9 +35,14 @@ $(document).ready(function() {    /*permet d'augmenter ma progress barre lorsque
           //     elem.style.backgroundColor = "green"
           //   }
           // }
+ 
 
-      if(percentage==100){setTimeout(function(){alert("félicitations");}, 1000);}  /* si le pourcentage arrive a 100% un pop up felicitation apparait au bout de 1s*/
-    }
+      if(percentage==100){setTimeout(function(){alert("Félicitations !");}, 1000);}
+      
+    
+        /* si le pourcentage arrive a 100% un pop up felicitation apparait au bout de 1s*/
+    } 
+    
     
     countChecked();
     $(":checkbox").click(countChecked);   /* sans ca ni le pourcent ni la progression apparait*/
@@ -46,7 +51,49 @@ $(document).ready(function() {    /*permet d'augmenter ma progress barre lorsque
 
  
 
+        /* JS POUR LE FORMULAIRE AU DESSUS DES TACHES */
 
 
+  $(function() {
+    // function and name of the form
+    $("form[name='registration']").validate({
 
-   
+      rules: {
+        // names on the left: values
+        // names on the right: rules
+        civilite: "required",
+        name: {
+            required: true,
+            minlength: 2
+        },
+        sexe: "required",
+        telephone: {
+          required: true,
+          minlength: 10,
+          maxlength: 10,
+        },
+        email: {
+          required: true,
+          email: true
+        },
+      },
+        // error messages
+        messages: {
+          name: " Veuillez saisir votre nom.",
+          sexe: " Veuillez saisir votre sexe.",
+          telephone: {
+            required: " Veuillez saisir votre téléphone.",
+            minlength: " Votre téléphone doit afficher 10 chiffres.",
+            maxlength: " Votre téléphone doit afficher 10 chiffres.",
+          },
+          
+  
+        },
+  
+        submitHandler: function(form) {
+          form.submit();
+          
+        }
+      });
+    });
+    
