ed5fbd37b66d54b4eae94665dae344cae2c0aca6
Eugenie94 <engie94400@gmail.com>
2020-09-24 02:54:06 +0200

    ajout commentaires

--- page1.js
+++ page1.js
@@ -3,26 +3,28 @@ $(document).ready(function() {    /*permet d'augmenter ma progress barre lorsque
     // get box count
     var count = 0;   
     var checked = 0;
-    function countBoxes() { 
+    function countBoxes() {          /*ca compte le nombre de box pour definir par la suite les pourcentage a divisé*/
       count = $("input[type='checkbox']").length;
       console.log(count);
     }
     
-    countBoxes();
+    countBoxes();   /*on rappelle la fonction */
     $(":checkbox").click(countBoxes);
     
     // count checks
     
-    function countChecked() {
+    function countChecked() {       /*fonction qui permet de compté les cases en fonction du pourcentage qui arrive et l'affiche ensuite*/
       checked = $("input:checked").length;
       
       var percentage = parseInt(((checked / count) * 100),10);
       $(".progressbar-bar").progressbar({
               value: percentage
           });
-      $(".progressbar-label").text(percentage + "%"); 
-      var elem = document.getElementById("mam");    /*permet que la couleur de progress augmente avec la progression*/
+      $(".progressbar-label").text(percentage + "%"); /*ce qui fait que le texte des pourcentage apparait*/
+      var elem = document.getElementById("label");    /*permet que la couleur de progress augmente avec la progression*/
       elem.style.width = percentage + "%";    /*aussi*/
+
+
 
           // if(percentage<25){              ce qui permet d'avoir un trio de couleur au fur et a mesure que le pourcentage augmente
           //   elem.style.backgroundColor = "red"
@@ -37,9 +39,8 @@ $(document).ready(function() {    /*permet d'augmenter ma progress barre lorsque
       if(percentage==100){setTimeout(function(){alert("félicitations");}, 1000);}  /* si le pourcentage arrive a 100% un pop up felicitation apparait au bout de 1s*/
     }
     
-    
     countChecked();
-    $(":checkbox").click(countChecked);
+    $(":checkbox").click(countChecked);   /* sans ca ni le pourcent ni la progression apparait*/
   });
 
 
